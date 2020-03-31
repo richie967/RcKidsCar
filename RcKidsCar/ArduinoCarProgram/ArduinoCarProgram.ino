32f439a05680b223943a75107cabd55e6bed9c7a
richie967 <rablackwell1@hotmail.com>
2020-03-31 23:33:24 +0100

    Update ArduinoCarProgram.ino
    Broke the Code by trying to add a gear function!

--- RcKidsCar/ArduinoCarProgram/ArduinoCarProgram.ino
+++ RcKidsCar/ArduinoCarProgram/ArduinoCarProgram.ino
@@ -1,13 +1,17 @@
 #include <Servo.h>
 
 const int DRIVE_MODE_PIN = 3;
-const int STEERING_SERVO_PIN = 11;
+const int STEERING_SERVO_PIN = 4; // Moved to a Non OWM Pin
 const int STEERING_INPUT_MANUAL_PIN = A0;
 const int STEERING_INPUT_REMOTE_PIN = 6;
-const int STEERING_CENTRE_MINIMUM = 87;
-const int STEERING_CENTRE_MAXIMUM = 93;
+const int STEERING_CENTRE_MINIMUM = 86;
+const int STEERING_CENTRE_MAXIMUM = 96;
 const int PARENTAL_OVERRIDE_PIN = 5;
 const int THROTTLE_INPUT_REMOTE_PIN = 9;
+const int THROTTLE_OUTPUT_PIN = 10;
+const int DRIVE_DIRECTION_FORWARD_PIN = 13;
+const int DRIVE_DIRECTION_REVERSE_PIN = 12;
+
 
 Servo _steeringServo;
 
@@ -20,6 +24,8 @@ void setup() {
   pinMode(STEERING_INPUT_REMOTE_PIN, INPUT);
   pinMode(PARENTAL_OVERRIDE_PIN, INPUT);
   pinMode(THROTTLE_INPUT_REMOTE_PIN, INPUT);
+  pinMode(DRIVE_DIRECTION_FORWARD_PIN, INPUT_PULLUP);
+  pinMode(DRIVE_DIRECTION_REVERSE_PIN, INPUT_PULLUP);
 
   _steeringServo.attach(STEERING_SERVO_PIN, 700, 2400);
   _steeringServo.write(90);
@@ -29,18 +35,30 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:#
+  // put your main code here, to run repeatedly:
+
   int parentalOverrideValue = pulseIn(PARENTAL_OVERRIDE_PIN, HIGH);
   bool isParentalOverride = parentalOverrideValue > 1300;
   bool isRemoteControl = digitalRead(DRIVE_MODE_PIN);
 
+  
+  char gear = readGearstick();
+  Serial.println(gear);
+
   int steeringAngle = 90;
   int throttlePosition = 0;
+
+  // If Remote control or parent override is on
+
   if (isRemoteControl || isParentalOverride)
   {
     steeringAngle = readRemoteSteeringAngle();
     throttlePosition = readRemoteThrottlePosition();
   }
+
+
+  // If Manual Mode
+
   else
   {
     steeringAngle = readManualSteeringAngle();
@@ -53,7 +71,15 @@ void loop() {
 
   //Serial.println(steeringAngle);
   //Serial.println(isParentalOverride);
-  Serial.println(throttlePosition);
+  //  Serial.print("Throttle Position= ");
+  //  Serial.print( throttlePosition);
+  //  Serial.print(" Steering Angle= ");
+  //  Serial.print( steeringAngle);
+  //  Serial.print(" Is Remote Control= ");
+  //  Serial.print(isRemoteControl);
+  //  Serial.print(" Is Parent Override= ");
+  //  Serial.print(isParentalOverride);
+  //  Serial.println();
 
   _steeringServo.write(steeringAngle);
 
@@ -62,12 +88,14 @@ void loop() {
 
 int readManualSteeringAngle()
 {
+  int steeringPosition = 90;
   int steeringValue = analogRead(STEERING_INPUT_MANUAL_PIN);
   return map(steeringValue, 0, 1023, 0, 180);
 }
 
 int readRemoteSteeringAngle()
 {
+  int steeringPosition = 90;
   int steeringValue = pulseIn(STEERING_INPUT_REMOTE_PIN, HIGH);
   return map(steeringValue, 990, 1990, 0, 180);
 }
@@ -101,4 +129,28 @@ int readRemoteThrottlePosition()
     }
   }
   return throttlePosition;
+}
+
+char readGearstick()
+{
+  //Determine Gearsstick potion
+  bool forwardValue = digitalRead(DRIVE_DIRECTION_FORWARD_PIN);
+  bool reverseValue = digitalRead(DRIVE_DIRECTION_REVERSE_PIN);
+  char gear = 0;
+
+  if (!forwardValue) // Runs if gearstick is forwards
+  {
+    return char gear = "Forward";
+
+  };
+  if (!reverseValue) // Runs if gearstick is reverse
+  {
+    return char gear = "Reverse";
+
+  };
+  if (forwardValue && reverseValue) // Runs if gearstick is neutral
+  {
+    return char gear = "Neutral";
+
+  };
 }
