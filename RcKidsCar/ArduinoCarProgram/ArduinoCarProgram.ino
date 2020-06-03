9c7c0cc41a4b61eaa26d557d161eb571b011df7d
richie967 <rablackwell1@hotmail.com>
2020-06-03 23:57:22 +0100

    Update ArduinoCarProgram.ino

--- RcKidsCar/ArduinoCarProgram/ArduinoCarProgram.ino
+++ RcKidsCar/ArduinoCarProgram/ArduinoCarProgram.ino
@@ -1,197 +1,387 @@
-#include <Servo.h>
+//#include <Servo.h>
 
-const int DRIVE_MODE_PIN = 3;
-const int STEERING_SERVO_PIN = 4; // Moved to a Non OWM Pin
-const int STEERING_INPUT_MANUAL_PIN = A0;
-const int STEERING_INPUT_REMOTE_PIN = 6;
-const int STEERING_CENTRE_MINIMUM = 86;
-const int STEERING_CENTRE_MAXIMUM = 96;
-const int PARENTAL_OVERRIDE_PIN = 5;
-const int THROTTLE_INPUT_REMOTE_PIN = 9;
-const int THROTTLE_INPUT_MANUAL_PIN = 2;
-const int THROTTLE_OUTPUT_PIN = 10;
-const int DRIVE_DIRECTION_FORWARD_PIN = 13;
-const int DRIVE_DIRECTION_REVERSE_PIN = 12;
+// pin configuration
+const int PIN_INPUT_INTERNAL_DRIVE_MODE = 8;
+const int PIN_INPUT_INTERNAL_GEAR_SELECTION_FORWARD = 13;
+const int PIN_INPUT_INTERNAL_GEAR_SELECTION_REVERSE = 12;
+const int PIN_INPUT_INTERNAL_PROXIMITY_TRIGGER = 7;
+const int PIN_INPUT_INTERNAL_PROXIMITY_ECHO = 11;
+const int PIN_INPUT_INTERNAL_THROTTLE = A0;
+const int PIN_INPUT_INTERNAL_STEERING_DATA = 4;
+const int PIN_INPUT_INTERNAL_STEERING_CLOCK = 2;
+const int PIN_INPUT_REMOTE_FAILSAFE = 10;
+const int PIN_INPUT_REMOTE_OVERRIDE = 5;
+const int PIN_INPUT_REMOTE_THROTTLE = 9;
+const int PIN_INPUT_REMOTE_STEERING = 6;
+const int PIN_OUTPUT_THROTTLE = 3;
+// const int PIN_OUTPUT_STEERING = 0;
 
-Servo _steeringServo;
+// remote reciever sends PWM signals in the range ~1000 to ~2000
+const int REMOTE_LOW_VALUE_MINIMUM = 950;
+const int REMOTE_LOW_VALUE_MAXIMUM = 1050;
+const int REMOTE_MIDDLE_VALUE_MINIMUM = 1450;
+const int REMOTE_MIDDLE_VALUE_MAXIMUM = 1550;
+const int REMOTE_HIGH_VALUE_MINIMUM = 1950;
+const int REMOTE_HIGH_VALUE_MAXIMUM = 2050;
+const int MANUAL_THROTTLE_VALUE_MINIMUM = 250;
+const int MANUAL_THROTTLE_VALUE_MAXIMUM = 750;
+const int MANUAL_THROTTLE_MAXIMUM_OUTPUT = 50;
+const int STEERING_ANGLE_CENTRE = 90;
+const int STEERING_ANGLE_MINIMUM = 45;
+const int STEERING_ANGLE_MAXIMUM = 135;
+const int REMOTE_STEERING_CENTRE_MINIMUM = 86;
+const int REMOTE_STEERING_CENTRE_MAXIMUM = 94;
+
+enum DriveMode
+{
+  Manual,
+  RemoteControl
+};
+
+enum DriveDevice
+{
+  Internal,
+  Remote,
+  None
+};
+
+enum Gear
+{
+  Forward,
+  Reverse,
+  Neutral
+};
+
+struct ControlState
+{
+  bool RemoteInUse;
+  bool Failsafe;
+  DriveMode InternalDriveMode;
+  DriveMode RemoteDriveMode;
+  DriveDevice DriveDevice;
+  int Proximity;
+  Gear Gear;
+  int SteeringAngle;
+  int Power;
+} currentState;
+
+int internalSteeringDataLast;
+//Servo steeringServo;
 
 void setup() {
-  // put your setup code here, to run once:
+  // configure input pins
+  pinMode(PIN_INPUT_INTERNAL_DRIVE_MODE, INPUT);
+  pinMode(PIN_INPUT_INTERNAL_GEAR_SELECTION_FORWARD, INPUT_PULLUP);
+  pinMode(PIN_INPUT_INTERNAL_GEAR_SELECTION_REVERSE, INPUT_PULLUP);
+  pinMode(PIN_INPUT_INTERNAL_PROXIMITY_TRIGGER, OUTPUT);
+  pinMode(PIN_INPUT_INTERNAL_PROXIMITY_ECHO, INPUT);
+  pinMode(PIN_INPUT_INTERNAL_THROTTLE, INPUT);
+  pinMode(PIN_INPUT_INTERNAL_STEERING_DATA, INPUT_PULLUP);
+  pinMode(PIN_INPUT_INTERNAL_STEERING_CLOCK, INPUT_PULLUP);
+  pinMode(PIN_INPUT_REMOTE_FAILSAFE, INPUT);
+  pinMode(PIN_INPUT_REMOTE_OVERRIDE, INPUT);
+  pinMode(PIN_INPUT_REMOTE_THROTTLE, INPUT);
+  pinMode(PIN_INPUT_REMOTE_STEERING, INPUT);
 
-  // Inputs
-  pinMode(DRIVE_MODE_PIN, INPUT);
-  pinMode(STEERING_INPUT_MANUAL_PIN, INPUT);
-  pinMode(STEERING_INPUT_REMOTE_PIN, INPUT);
-  pinMode(PARENTAL_OVERRIDE_PIN, INPUT);
-  pinMode(THROTTLE_INPUT_REMOTE_PIN, INPUT);
-  pinMode(DRIVE_DIRECTION_FORWARD_PIN, INPUT_PULLUP);
-  pinMode(DRIVE_DIRECTION_REVERSE_PIN, INPUT_PULLUP);
-  pinMode(THROTTLE_INPUT_MANUAL_PIN, INPUT_PULLUP);
+  // configure output pins
+//  steeringServo.attach(PIN_OUTPUT_THROTTLE, 700, 2400);
+//  steeringServo.write(90);
 
-
-  // Outputs
-  _steeringServo.attach(STEERING_SERVO_PIN, 700, 2400);
-  _steeringServo.write(90);
+  internalSteeringDataLast = digitalRead(PIN_INPUT_INTERNAL_STEERING_DATA);
 
   Serial.begin(9600);
-  Serial.println("Welcome to the Arduino Car program");
+  Serial.println("Arduino Car Started. Brmmm Brmmm.");
 }
 
-void loop() {
+void loop()
+{
+  // read all inputs
+  refreshCurrentState();
 
-  // In Car Controls
-  int parentalOverrideValue = pulseIn(PARENTAL_OVERRIDE_PIN, HIGH);
+//  steeringServo.write(currentState.SteeringAngle);
 
-  // Remote Controls
-  bool isParentalOverride = parentalOverrideValue > 1300; // This reads a button on the Remote control that determines if the parent should override the cr controls
-  bool isRemoteControl = digitalRead(DRIVE_MODE_PIN);
+  // update output
+  Serial.println("");
 
-  // Variables that get set by the remote control or the in car controls
-  int steeringAngle = 90;
-  int throttlePosition = 0;
+  Serial.print("Remote in use: ");
+  Serial.println(currentState.RemoteInUse);
 
-  // Variables that get set from the in car controls
-  int gear = readGearstick();
+  Serial.print("Failsafe: ");
+  Serial.println(currentState.Failsafe);
 
-  // If Remote control or parent override is on
+  Serial.print("Remote drive mode: ");
+  Serial.println(currentState.RemoteDriveMode);
 
-  if (isRemoteControl || isParentalOverride)
+  Serial.print("Internal drive mode: ");
+  Serial.println(currentState.InternalDriveMode);
+
+  Serial.print("Drive device: ");
+  Serial.println(currentState.DriveDevice);
+
+  Serial.print("Proximity: ");
+  Serial.println(currentState.Proximity);
+
+  Serial.print("Gear selection: ");
+  Serial.println(currentState.Gear);
+
+  Serial.print("Power: ");
+  Serial.println(currentState.Power);
+
+  Serial.print("Steering angle: ");
+  Serial.println(currentState.SteeringAngle);
+
+  delay(2000);
+}
+
+void refreshCurrentState()
+{
+  // RC status and internal drive mode switch required to determine drive device
+  currentState.RemoteInUse = readRemoteInUse();
+  currentState.Failsafe = readFailsafe();
+  currentState.RemoteDriveMode = readRemoteDriveMode();
+  currentState.InternalDriveMode = readInternalDriveMode();
+
+  // can now determine drive device
+  currentState.DriveDevice = determineDriveDevice();
+
+  // once drive device is known drive inputs can be calculated
+  currentState.Proximity = readProximity();
+  currentState.Gear = readGearPosition();
+  currentState.Power = readThrottlePosition();
+  currentState.SteeringAngle = readSteeringAngle(currentState.SteeringAngle);
+}
+
+bool readRemoteInUse()
+{
+  // when remote receiver started without remote connected remote receiver outputs 0, otherwise LOW, MIDDLE, HIGH values as described in constants
+  // failsafe value is LOW
+  int pinValue = pulseIn(PIN_INPUT_REMOTE_FAILSAFE, HIGH);
+  return pinValue > 0;
+}
+
+bool readFailsafe()
+{
+  // see readRemoteInUse for failsafe value description
+  int pinValue = pulseIn(PIN_INPUT_REMOTE_FAILSAFE, HIGH);
+  return pinValue >= REMOTE_LOW_VALUE_MINIMUM && pinValue <= REMOTE_LOW_VALUE_MAXIMUM;
+}
+
+DriveMode readRemoteDriveMode()
+{
+  // PWM signal from remote sends one of three values 0 (remote not connected), ~1000 (LOW, switch off), ~2000 (HIGH, switch on)
+  int pinValue = pulseIn(PIN_INPUT_REMOTE_OVERRIDE, HIGH);
+  
+  if (pinValue >= REMOTE_HIGH_VALUE_MINIMUM && pinValue <= REMOTE_HIGH_VALUE_MAXIMUM)
   {
-    steeringAngle = readRemoteSteeringAngle();
-    throttlePosition = readRemoteThrottlePosition();
-    Serial.println("Remote or override is on");
-    Serial.println(throttlePosition);
+    return RemoteControl;
   }
 
+  return Manual;
+}
 
-  // If Manual Mode
+DriveMode readInternalDriveMode()
+{
+  int pinValue = digitalRead(PIN_INPUT_INTERNAL_DRIVE_MODE);
+  return static_cast<DriveMode>(pinValue);
+}
 
+DriveDevice determineDriveDevice()
+{
+  if (currentState.RemoteInUse)
+  {
+    if (currentState.Failsafe)
+    {
+      return None;
+    }
+
+    if (currentState.RemoteDriveMode == RemoteControl)
+    {
+      return Remote;
+    }
+
+    return Internal;
+  }
+
+  if (currentState.InternalDriveMode == Manual)
+  {
+    return Internal;
+  }
+
+  return None;
+}
+
+int readProximity()
+{
+  digitalWrite(PIN_INPUT_INTERNAL_PROXIMITY_TRIGGER, LOW);
+  delayMicroseconds(2);
+  digitalWrite(PIN_INPUT_INTERNAL_PROXIMITY_TRIGGER, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_INPUT_INTERNAL_PROXIMITY_TRIGGER, LOW);
+
+  int duration = pulseIn(PIN_INPUT_INTERNAL_PROXIMITY_ECHO, HIGH);
+
+  return (duration / 2) * 0.0343;
+}
+
+Gear readGearPosition()
+{
+  if (currentState.DriveDevice == None)
+  {
+    return Neutral;
+  }
+  
+  if (currentState.DriveDevice == Manual)
+  {
+    return readGearPositionManual();
+  }
+  
+  return readGearPositionRemote();
+}
+
+Gear readGearPositionManual()
+{
+  int forwardPinValue = !digitalRead(PIN_INPUT_INTERNAL_GEAR_SELECTION_FORWARD);
+  int reversePinValue = !digitalRead(PIN_INPUT_INTERNAL_GEAR_SELECTION_REVERSE);
+
+  if (forwardPinValue)
+  {
+    return Forward;
+  }
+  else if (reversePinValue)
+  {
+    return Reverse;
+  }
+
+  return Neutral;
+}
+
+Gear readGearPositionRemote()
+{
+  int throttleValue = pulseIn(PIN_INPUT_REMOTE_THROTTLE, HIGH);
+
+  if (throttleValue > REMOTE_MIDDLE_VALUE_MAXIMUM)
+  {
+    return Forward;
+  }
+  else if (throttleValue < REMOTE_MIDDLE_VALUE_MINIMUM)
+  {
+    return Reverse;
+  }
+
+  return Neutral;
+}
+
+int readThrottlePosition()
+{
+  int throttlePosition = 0;
+  int throttleMax = 100;
+
+  if (currentState.DriveDevice == Manual)
+  {
+    throttleMax = MANUAL_THROTTLE_MAXIMUM_OUTPUT;
+    throttlePosition = readThrottlePositionManualRaw();
+  }
   else
   {
-    steeringAngle = readManualSteeringAngle();
-    Serial.println("Gear in main loop function");
-    Serial.println(gear);
-    
-    Serial.println("Throttle Position at top of function");
-    Serial.println(throttlePosition);
-    if (gear == 1 && throttlePosition == 0) // Forward gear selected and foot pedal pressed down
-    {
-      throttlePosition = 25;
-    };
-    if (gear == 2 && throttlePosition == 0) // Reverse gear selected and foot pedal pressed down
-    {
-      throttlePosition = -25;
-    };
-    if (gear == 3 && throttlePosition == 1) // Neutral is selected, set throttle to disabled
-    {
-    Serial.println("Throttle Disabled");
-      throttlePosition = 0;
-    };
-
-    
-
-    Serial.println("Manual Throttle is on");
-    Serial.println("Throttle Position at bottom of function");
-    Serial.println(throttlePosition);
+    throttlePosition = readThrottlePositionRemoteRaw();
   }
 
-  if (steeringAngle >= STEERING_CENTRE_MINIMUM && steeringAngle <= STEERING_CENTRE_MAXIMUM)
+  if (throttlePosition < 0)
   {
-    steeringAngle = 90;
+    return 0;
   }
-
-  //Serial.println(steeringAngle);
-  //Serial.println(isParentalOverride);
-  //  Serial.print("Throttle Position= ");
-  //  Serial.print( throttlePosition);
-  //  Serial.print(" Steering Angle= ");
-  //  Serial.print( steeringAngle);
-  //  Serial.print(" Is Remote Control= ");
-  //  Serial.print(isRemoteControl);
-  //  Serial.print(" Is Parent Override= ");
-  //  Serial.print(isParentalOverride);
-  //  Serial.println();
-  _steeringServo.write(steeringAngle);
-  delay(3000);
-}
-
-// End of Main Loop Function
-
-int readManualSteeringAngle()
-{
-  int steeringPosition = 90;
-  int steeringValue = analogRead(STEERING_INPUT_MANUAL_PIN);
-  return map(steeringValue, 0, 1023, 0, 180);
-}
-
-int readRemoteSteeringAngle()
-{
-  int steeringPosition = 90;
-  int steeringValue = pulseIn(STEERING_INPUT_REMOTE_PIN, HIGH);
-  return map(steeringValue, 990, 1990, 0, 180);
-}
-
-int readManualThrottlePosition()
-{
-  int throttlePosition = 0;
-  int manualthrottlePosition = digitalRead(THROTTLE_INPUT_MANUAL_PIN);
-  Serial.println("Manual Throtte Position From sub function");
-  Serial.println("manualthrottlePosition");
-  return throttlePosition = manualthrottlePosition;
-  
-}
-
-int readRemoteThrottlePosition()
-{
-  int throttlePosition = 0;
-  int throttleValue = pulseIn(THROTTLE_INPUT_REMOTE_PIN, HIGH);
-  if (throttleValue <= 1450)
+  else if (throttlePosition > throttleMax)
   {
-    throttlePosition = map(throttleValue, 990, 1450, -100, 0);
-    if (throttlePosition < -100)
-    {
-      throttlePosition = -100;
-    }
-    else if (throttlePosition > 0)
-    {
-      throttlePosition = 0;
-    }
+    return throttleMax;
   }
-  else if (throttleValue >= 1500)
-  {
-    throttlePosition = map(throttleValue, 1500, 1980, 0, 100);
-    if (throttlePosition < 0)
-    {
-      throttlePosition = 0;
-    }
-    else if (throttlePosition > 100)
-    {
-      throttlePosition = 100;
-    }
-  }
+
   return throttlePosition;
 }
 
-int readGearstick()
+int readThrottlePositionManualRaw()
 {
-  //Determine Gearsstick potion
-  bool forwardValue = digitalRead(DRIVE_DIRECTION_FORWARD_PIN);
-  bool reverseValue = digitalRead(DRIVE_DIRECTION_REVERSE_PIN);
-  int gear = 0;
+  int throttleValue = analogRead(PIN_INPUT_INTERNAL_THROTTLE);
+  return map(throttleValue, MANUAL_THROTTLE_VALUE_MINIMUM, MANUAL_THROTTLE_VALUE_MAXIMUM, 0, MANUAL_THROTTLE_MAXIMUM_OUTPUT);
+}
 
-  if (!forwardValue) // Runs if gearstick is forwards
-  {
-    return gear = 1;
-  }
-  if (!reverseValue) // Runs if gearstick is reverse
-  {
-    return gear = 2;
-  }
-  if (forwardValue && reverseValue) // Runs if gearstick is neutral
-  {
-    return gear = 3;
-  };
-  Serial.println("Gear from Function");
-  Serial.println(gear);
+int readThrottlePositionRemoteRaw()
+{
+  int throttleValue = pulseIn(PIN_INPUT_REMOTE_THROTTLE, HIGH);
 
+  if (throttleValue <= REMOTE_MIDDLE_VALUE_MINIMUM)
+  {
+    // negative throttle (reverse)
+    return map(throttleValue, REMOTE_LOW_VALUE_MAXIMUM, REMOTE_MIDDLE_VALUE_MINIMUM, -100, 0) * -1;
+  }
+  else if (throttleValue >= 1500)
+  {
+    // positive throttle (forward)
+    return map(throttleValue, REMOTE_MIDDLE_VALUE_MAXIMUM, REMOTE_HIGH_VALUE_MINIMUM, 0, 100);
+  }
+
+  return 0;
+}
+
+int readSteeringAngle(int steeringLast)
+{
+  int steeringAngle = STEERING_ANGLE_CENTRE;
+
+  if (currentState.DriveDevice == None)
+  {
+    return STEERING_ANGLE_CENTRE;
+  }
+  else if (currentState.DriveDevice == Manual)
+  {
+    steeringAngle = readSteeringAngleManualRaw(steeringLast);
+  }
+  else
+  {
+    steeringAngle = readSteeringAngleRemoteRaw();
+  }
+  
+  if (steeringAngle < STEERING_ANGLE_MINIMUM)
+  {
+    return STEERING_ANGLE_MINIMUM;
+  }
+  else if (steeringAngle > STEERING_ANGLE_MAXIMUM)
+  {
+    return STEERING_ANGLE_MAXIMUM;
+  }
+
+  return steeringAngle;
+}
+
+int readSteeringAngleManualRaw(int steeringLast)
+{
+  int data = digitalRead(PIN_INPUT_INTERNAL_STEERING_DATA);
+
+  if (data != internalSteeringDataLast)
+  {
+    internalSteeringDataLast = data;
+    int clock = digitalRead(PIN_INPUT_INTERNAL_STEERING_CLOCK);
+
+    if (clock != data)
+    {
+      return steeringLast + 4;
+    }
+    else
+    {
+      return steeringLast - 4;
+    }
+  }
+
+  return steeringLast;
+}
+
+int readSteeringAngleRemoteRaw()
+{
+  int steeringValue = pulseIn(PIN_INPUT_REMOTE_STEERING, HIGH);
+  int steeringAngle = map(steeringValue, REMOTE_LOW_VALUE_MAXIMUM, REMOTE_HIGH_VALUE_MINIMUM, STEERING_ANGLE_MINIMUM, STEERING_ANGLE_MAXIMUM);
+
+  if (steeringAngle >= REMOTE_STEERING_CENTRE_MINIMUM && steeringAngle <= REMOTE_STEERING_CENTRE_MAXIMUM)
+  {
+    return STEERING_ANGLE_CENTRE;
+  }
+
+  return steeringAngle;
 }
